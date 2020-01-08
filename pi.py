002d41c077c4383a31a7ef432cae06dfac20a39a
Will <williampeterjane@gmail.com>
2020-01-07 22:13:49 -0800

    Adding buzzer example.

--- pi.py
+++ pi.py
@@ -7,13 +7,24 @@ GPIO.setmode(GPIO.BCM)
 
 LED_PIN = 18
 
-GPIO.setup(LED_PIN,GPIO.OUT)
+GPIO.setup(LED_PIN, GPIO.OUT)
 
 try:
     while True:
-        GPIO.output(LED_PIN,True)
+        GPIO.output(LED_PIN, True)
         time.sleep(1)
-        GPIO.output(LED_PIN,False)
+        GPIO.output(LED_PIN, False)
         time.sleep(1)
 finally:
     GPIO.cleanup()
+
+    
+def buzzer():
+    middle_c = 261
+    p = GPIO.PWM(LED_PIN, middle_c)
+    p.start(50)
+    
+    while True:
+        for freq in range(200, 400, 10):
+            p.ChangeFrequency(freq)
+            time.sleep(0.1)
